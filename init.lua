3c5e0a151d10c4fd4bf522c1f8bcf7333839d1ce
flath <flath1995@gmail.com>
2024-05-06 17:27:38 +0200

    update :ex command to leader pv

--- init.lua
+++ init.lua
@@ -68,10 +68,12 @@ vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+--  `:help vim.keymap.set()`
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>pv', '<cmd>Ex<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', 'gD', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
