de889dea72e73826c093a143100a7a01c2888da4
flath <flath1995@gmail.com>
2026-02-02 17:11:03 +0100

    rework bindings for neotree to gg

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'gg', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['gg'] = 'close_window',
         },
       },
     },
